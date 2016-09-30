2d7838d869e7f0645644135f15f6da7fc3626cd7
Philhawk <pkjacob@gmail.com>
2016-09-30 11:15:42 -0400

    responsive

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.set('view cache', false);
 
 
 // all environments
+// THIS IS NEW
 app.set('port', process.env.PORT || 3000);
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -33,7 +34,7 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(
   sass({
-    src: __dirname + '/assets', //where the sass files are 
+    src: __dirname + '/assets', //where the sass files are
     dest: __dirname + '/public', //where css should go
     debug: true
   })
@@ -52,5 +53,3 @@ app.get('/contact', routes.contact);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-
